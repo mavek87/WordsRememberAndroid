374bf5fbae163207a86d660c10499d4e9f20c93b
veroni <matver87@gmail.com>
2017-02-08 17:50:15 +0100

    started to work on DictionaryManagementPresenterTest

--- app/src/test/java/com/matteoveroni/wordsremember/dictionary/DictionaryManagementPresenterTest.java
+++ app/src/test/java/com/matteoveroni/wordsremember/dictionary/DictionaryManagementPresenterTest.java
@@ -1,7 +1,18 @@
 package com.matteoveroni.wordsremember.dictionary;
 
+import com.matteoveroni.wordsremember.dictionary.factories.DictionaryManagementPresenterFactory;
+import com.matteoveroni.wordsremember.dictionary.model.DictionaryDAO;
+import com.matteoveroni.wordsremember.dictionary.presenter.DictionaryManagementPresenter;
+import com.matteoveroni.wordsremember.pojo.Word;
+
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * @author Matteo Veroni
@@ -10,5 +21,17 @@ import org.mockito.junit.MockitoRule;
 public class DictionaryManagementPresenterTest {
 
     public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    DictionaryManagementPresenter presenter = new DictionaryManagementPresenterFactory().create();
+    @Mock
+    DictionaryManagementView view;
+    @Mock
+    DictionaryDAO model;
+
+    @Test
+    public void onCreateVocableRequestPresenterOrderToViewToGoToManipulationView() {
+        presenter.onCreateVocableRequest();
+//        Mockito.verify(view).goToManipulationView(Matchers.any(Word.class));
+    }
 
 }
