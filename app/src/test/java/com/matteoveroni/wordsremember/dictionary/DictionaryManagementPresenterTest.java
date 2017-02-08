224eab7289cb397451cb2913d69f25032d5b8ffe
veroni <matver87@gmail.com>
2017-02-08 16:02:03 +0100

    Updated gradle dependencies and created DictionaryManagementPresenterTest class

---
+++ app/src/test/java/com/matteoveroni/wordsremember/dictionary/DictionaryManagementPresenterTest.java
@@ -0,0 +1,14 @@
+package com.matteoveroni.wordsremember.dictionary;
+
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+/**
+ * @author Matteo Veroni
+ */
+
+public class DictionaryManagementPresenterTest {
+
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+}
