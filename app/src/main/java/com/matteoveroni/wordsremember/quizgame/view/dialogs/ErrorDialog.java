6f42a33038c75d80103f9e26d1f87c11371c83fd
Matteo Veroni <matver87@gmail.com>
2017-06-28 15:11:41 +0200

    refactoring of quiz game dialogs

--- app/src/main/java/com/matteoveroni/wordsremember/quizgame/view/dialogs/ErrorDialog.java
+++ app/src/main/java/com/matteoveroni/wordsremember/quizgame/view/dialogs/ErrorDialog.java
@@ -4,22 +4,15 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
 /**
  * @author Matteo Veroni
  */
 
-public class ErrorDialog extends DialogFragment {
+public class ErrorDialog extends BaseTitleMessageDialog {
 
     public static final String TAG = ErrorDialog.class.getSimpleName();
-
-    private String title;
-    private static final String DIALOG_TITLE_KEY = "title";
-
-    private String message;
-    private static final String DIALOG_MESSAGE_KEY = "message";
 
     public interface ErrorDialogListener {
         void confirmErrorDialogAction();
@@ -27,22 +20,8 @@ public class ErrorDialog extends DialogFragment {
 
     public static ErrorDialog newInstance(String title, String message) {
         ErrorDialog errorDialogFragment = new ErrorDialog();
-
-        Bundle args = new Bundle();
-        args.putString(DIALOG_TITLE_KEY, title);
-        args.putString(DIALOG_MESSAGE_KEY, message);
-        errorDialogFragment.setArguments(args);
-
+        initDialogData(title, message, errorDialogFragment);
         return errorDialogFragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // TODO: check if setRetainInstanceIsNeeded
-        setRetainInstance(true);
-        title = getArguments().getString(DIALOG_TITLE_KEY);
-        message = getArguments().getString(DIALOG_MESSAGE_KEY);
     }
 
     @NonNull
@@ -62,14 +41,5 @@ public class ErrorDialog extends DialogFragment {
         dialog.setCancelable(false);
         dialog.setCanceledOnTouchOutside(false);
         return dialog;
-    }
-
-    @Override
-    public void onDestroyView() {
-        Dialog dialog = getDialog();
-        if (dialog != null && getRetainInstance()) {
-            dialog.setDismissMessage(null);
-        }
-        super.onDestroyView();
     }
 }
--- app/src/main/java/com/matteoveroni/wordsremember/quizgame/view/dialogs/GameResultDialog.java
+++ app/src/main/java/com/matteoveroni/wordsremember/quizgame/view/dialogs/GameResultDialog.java
@@ -2,29 +2,17 @@ package com.matteoveroni.wordsremember.quizgame.view.dialogs;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AlertDialog;
-
-import com.matteoveroni.wordsremember.R;
-import com.matteoveroni.wordsremember.quizgame.pojos.Quiz;
 
 /**
  * @author Matteo Veroni
  */
 
-public class GameResultDialog extends DialogFragment {
+public class GameResultDialog extends BaseTitleMessageDialog {
 
     public static final String TAG = GameResultDialog.class.getSimpleName();
-
-    private String title;
-    private static final String DIALOG_TITLE_KEY = "title";
-
-    private String message;
-    private static final String DIALOG_MESSAGE_KEY = "message";
 
     public interface GameResultDialogListener {
         void confirmGameResultDialogAction();
@@ -32,22 +20,8 @@ public class GameResultDialog extends DialogFragment {
 
     public static GameResultDialog newInstance(String title, String message) {
         GameResultDialog dialogFragment = new GameResultDialog();
-
-        Bundle args = new Bundle();
-        args.putString(DIALOG_TITLE_KEY, title);
-        args.putString(DIALOG_MESSAGE_KEY, message);
-        dialogFragment.setArguments(args);
-
+        initDialogData(title, message, dialogFragment);
         return dialogFragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // TODO: check if setRetainInstanceIsNeeded
-        setRetainInstance(true);
-        title = getArguments().getString(DIALOG_TITLE_KEY);
-        message = getArguments().getString(DIALOG_MESSAGE_KEY);
     }
 
     @NonNull
@@ -67,14 +41,5 @@ public class GameResultDialog extends DialogFragment {
         dialog.setCancelable(false);
         dialog.setCanceledOnTouchOutside(false);
         return dialog;
-    }
-
-    @Override
-    public void onDestroyView() {
-        Dialog dialog = getDialog();
-        if (dialog != null && getRetainInstance()) {
-            dialog.setDismissMessage(null);
-        }
-        super.onDestroyView();
     }
 }
